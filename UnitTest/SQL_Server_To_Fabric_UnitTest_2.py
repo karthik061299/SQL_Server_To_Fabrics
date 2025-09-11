9d3538545b7f92474a86166f6ecaaefbbe04a38f
karthik061299 <karthikeyancoc07@gmail.com>
2025-09-11 15:44:41 +0530

    Delete UnitTest/SQL_Server_To_Fabric_UnitTest_2.py

--- UnitTest/SQL_Server_To_Fabric_UnitTest_2.py
+++
@@ -1,6 +0,0 @@
-# Import integration tests from separate file
-from SQL_Server_To_Fabric_UnitTest_2_integration import TestIntegrationUspSemanticClaimTransactionMeasuresData
-
-if __name__ == "__main__":
-    # Run tests
-    pytest.main(["-v", "--tb=short"])
