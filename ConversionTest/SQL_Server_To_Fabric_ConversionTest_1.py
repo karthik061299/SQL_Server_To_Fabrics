d01cb5ac9f84ac9fa98ec6a71c9c78652256c856
karthik061299 <karthikeyancoc07@gmail.com>
2025-09-11 20:00:27 +0530

    Add or update file: SQL_Server_To_Fabric_ConversionTest_1.py

---
+++ ConversionTest/SQL_Server_To_Fabric_ConversionTest_1.py
@@ -0,0 +1,11 @@
+_____________________________________________
+## *Author*: AAVA
+## *Created on*: 
+## *Description*: Comprehensive test cases and Pytest script to validate SQL Server-to-Fabric SQL conversion for uspSemanticClaimTransactionMeasuresData
+## *Version*: 1 
+## *Updated on*: 
+_____________________________________________
+
+import pytest
+import pandas as pd
+from datetime import datetime
