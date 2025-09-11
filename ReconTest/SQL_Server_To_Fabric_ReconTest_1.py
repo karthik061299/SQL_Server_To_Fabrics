d1f9ee1a3bea5616920ef314b5217c7848f6a5fd
karthik061299 <karthikeyancoc07@gmail.com>
2025-09-11 15:52:48 +0530

    Add or update file: SQL_Server_To_Fabric_ReconTest_1.py

---
+++ ReconTest/SQL_Server_To_Fabric_ReconTest_1.py
@@ -0,0 +1,9 @@
+_____________________________________________
+## *Author*: AAVA
+## *Created on*:   
+## *Description*:   SQL Server to Fabric migration validation for uspSemanticClaimTransactionMeasuresData
+## *Version*: 1 
+## *Updated on*: 
+_____________________________________________
+
+'''This script validates the migration of uspSemanticClaimTransactionMeasuresData from SQL Server to Fabric.'''
